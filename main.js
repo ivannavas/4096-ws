43d3401743ab3a428a2996d8c056ac40557e2fe8
ivannavasss <ivannavas10@gmail.com>
2023-01-16 16:30:48 +0100

    Basic funcionality

---
+++ main.js
@@ -0,0 +1,56 @@
+import { WebSocketServer } from "ws";
+import { v4 as uuidv4 } from 'uuid';
+
+const wss = new WebSocketServer({ port: 8000 });
+const clients = new Map();
+const rooms = [];
+let currentRoom = 0;
+let currentRoomPlayers = 0;
+const roomLimit = 16;
+
+const getRoom = () => {
+    if (currentRoomPlayers >= roomLimit) {
+        currentRoom++;
+        currentRoomPlayers = 1;
+    } else {
+        currentRoomPlayers++;
+    }
+    return currentRoom;
+};
+const broadcastNewState = (room, id, state) => {
+    rooms[room].forEach(playerWs => {
+        playerWs.send(JSON.stringify({
+            "id": id,
+            "state": state
+        }));
+    });
+};
+
+wss.on('connection', (ws, req) => {
+    const clientId = uuidv4();
+    const room = getRoom();
+
+    console.log(req.socket.remoteAddress + " connected to room " + room);
+
+    clients.set(ws, {
+        id: clientId,
+        room: room,
+        state: {}
+    });
+
+    if (rooms[room]) {
+        rooms[room].push(ws);
+    } else {
+        rooms.push([]);
+    }
+
+    if (rooms[currentRoom].length > 1) {
+        broadcastNewState(currentRoom, clientId, {});
+    }
+
+    ws.on('message', (stateAsString) => {
+        const sender = clients.get(ws);
+        sender.state = JSON.parse(stateAsString);
+        broadcastNewState(sender.room, sender.id, sender.state);
+    });
+});
